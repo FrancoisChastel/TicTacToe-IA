d88226728d0b38ee6ff5fc6372f471343c314d4c
François Chastel <francois@chastel.co>
2017-10-23 10:36:41 +0200

    Modifying - minimax

--- player.cpp
+++ player.cpp
@@ -8,11 +8,8 @@ namespace TICTACTOE
 GameState Player::play(const GameState &pState,const Deadline &pDue)
 {
     Model* model = new Model((TICTACTOE::Cell) pState.getNextPlayer());
-    model->obtainBestMove(pState);
 
-    std::vector<TICTACTOE::GameState> pMoves;
-    pState.findPossibleMoves(pMoves);
-    return pMoves[0];
+    return model->obtainBestMove(pState);
 }
 
 /*namespace TICTACTOE*/ }
